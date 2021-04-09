f4aaeb44975a3c6f1f84d678747bf26f9c051019
asiahpirani <fotuhisiahpi@wisc.edu>
2021-04-09 21:15:52 +0430

    Update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
-# Shows some running examples for DRMN.
+
+exe=./learnDRMN
+
+indir=example_input
+
+suf=atac_qmotif_chromatin_9marks
+k=7
+v=0
+p1=10
+p2=50
+p3=40
+
+order=${indir}/order.txt
+ogids=${indir}/ogids.txt
+tree=${indir}/tree.txt
+conf=${indir}/${suf}_k${k}/config.txt
+src=esc
 
 out=example_out
-mkdir -p $out
+mkdir -p ${out}
 
-# Number of clusters
-k=3
-
-### Run DRMN with all 3 cell types. Learn one model from all data. ###
-./learnDRMN example_input/celltype_order.txt example_input/ogids_expr_100.txt null $k example_input/lineage_tree.txt example_input/histonly_k${k}_config.txt none $out learn ips uniform 0.8 selfInit
-
-exit
-
-##  Run fold 0 out of {0,1,2} of cross-validation
-seed=12345
-./learnDRMN example_input/celltype_order.txt example_input/ogids_expr_100.txt null $k example_input/lineage_tree.txt example_input/histonly_k${k}_config.txt none $out learnCV:0:${seed} ips uniform 0.8 selfInit
-
-exit
-
-### Run DRMN on one cell type only.
-mycell=mef
-./learnDRMN example_input/celltype_order.txt example_input/ogids_expr_100.txt null $k example_input/${cell}_tree.txt example_input/histonly_k${k}_config.txt none $out learn $mycell uniform 0.8 selfInit
-
-
+$exe ${order} ${ogids} null ${k} ${tree} ${conf} none ${out} learnCV:${v}:12345 ${src} uniform 0.80 selfInit LEASTFUSED ${p1} ${p2} ${p3} 
